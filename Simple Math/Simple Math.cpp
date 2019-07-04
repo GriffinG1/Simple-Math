fc66dcd7f265f392269c8e2c2a59312d6d3e4e1c
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2019-07-04 17:02:19 -0600

    Cleanup

--- Simple Math/Simple Math.cpp
+++ Simple Math/Simple Math.cpp
@@ -12,15 +12,19 @@
 
 const double clothingAndGardenTax = 0.06, beautyTax = 0.07, groceryAndSchoolTax = 0.03, tobaccoTax = 0.1;
 
-// Handles switch statement, including converting categories to int values
+// computes tax
 double computeTax(double d, const std::string& str) {
-	int switchVal;
-	if (str == "clothing" || str == "garden") switchVal = 1;
-	else if (str == "beauty") switchVal = 2;
-	else if (str == "grocery" || str == "school") switchVal = 3;
-	else if (str == "tobacco") switchVal = 4;
-	else switchVal = 0;
-	switch (switchVal) {
+	int categoryAsInt;
+
+	// converts categories to int
+	if (str == "clothing" || str == "gardening") categoryAsInt = 1;
+	else if (str == "beauty") categoryAsInt = 2;
+	else if (str == "grocery" || str == "school") categoryAsInt = 3;
+	else if (str == "tobacco") categoryAsInt = 4;
+	else categoryAsInt = 0;
+
+	// returns taxed price of each item based on category
+	switch (categoryAsInt) {
 		case 1:
 			return d + d * clothingAndGardenTax;
 		case 2:
@@ -40,39 +44,46 @@ int main()
 	std::string category;
 	std::vector<std::pair<double, std::string>> cost;
 	double finalCost = 0;
-	size_t dividerLength = 0;
+	size_t dividerLength = 1;
 
 	// Set decimal limits and give instructions for finishing input
 	std::cout << std::fixed << std::showpoint << std::setprecision(2);
 	std::cout << "Enter 'Finish' as a category to see your final total.\n" << std::endl;
+	std::cout << "Categories:\n" << "\tClothing\n" << "\tBeauty\n" << "\tGrocery\n" <<
+		"\tGardening\n" << "\tSchool\n" << "\tTobacco\n" << std::endl;
 
 	do { // Loops until "finish" is inputted
 		double price, taxPrice;
-		std::cout << "Categories:\n" << "\tClothing\n" << "\tBeauty\n" << "\tGrocery\n" <<
-			"\tGardening\n" << "\tSchool\n" << "\tTobacco\n\n" <<
-			"Enter the most appropriate of the above categories for the item: ";
+
+		// get category
+		std::cout << "Enter the most appropriate of the above categories for the item: ";
 		std::cin >> category;
 		std::transform(category.begin(), category.end(), category.begin(), ::tolower); // lowercase input string to make it idiot proof
 		std::cout << std::endl;
 
-		if (category == "finish") break;
+		if (category == "finish") break; // ends while loop early
+		
+		// get price
 		std::cout << "Enter price of item: ";
 		std::cin >> price;
 		std::cout << std::endl;
 
+		// Find tax price, then add the new pair to the vector
 		taxPrice = computeTax(price, category);
 		cost.emplace_back(taxPrice, category);
+
+		std::cout << "$" << taxPrice << " - item total\n" << std::endl;
 	} while (category != "finish");
 
 	for (std::pair<double, std::string> p : cost) { // Adds up total cost
 		finalCost += p.first;
-		if (p.second.length() + 13 > dividerLength) dividerLength = p.second.length() + 13;
-		std::cout << "$" << p.first << " - " << p.second << " item" << std::endl;
+		if (p.second.length() + 13 > dividerLength) dividerLength = p.second.length() + 13; // updates divider length to max
+		std::cout << "$" << p.first << " - " << p.second << " item" << std::endl; // outputs each taxed price with category
 	}
 
-	for (size_t i = 0; i < dividerLength+1; i++) printf("-"); printf("\n");
+	for (size_t i = 0; i < dividerLength; i++) printf("-"); printf("\n"); // outputs divider
 
-	std::cout << "$" << finalCost << " - " << "Total cost\n" << std::endl;
+	std::cout << "$" << finalCost << " - " << "total cost\n" << std::endl; // outputs final cost
 
 	// Wait for user input before closing
 	std::cout << "Press any key to close the program.";
